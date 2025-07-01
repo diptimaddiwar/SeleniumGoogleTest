3d1e053d95d2f3a5d4436bd337e2765715f618f8
Dipti Maddiwar <diptimaddiwar@gmail.com>
2025-07-01 15:28:41 -0400

    adding Myclass file

--- src/test/java/com/First/Hello.java
+++ src/test/java/com/First/Hello.java
@@ -1,5 +1,12 @@
 package com.First;
 
+import org.testng.annotations.Test;
+
 public class Hello {
 
+	@Test
+	public void hello()
+	{
+		System.out.println("Hello World");
+	}
 }
---
+++ src/test/java/com/First/MyClass.java
@@ -0,0 +1,11 @@
+package com.First;
+
+import org.testng.annotations.Test;
+
+public class MyClass {
+
+	@Test
+	public void add() {
+    System.out.println("I am inside MyClass");
+	}
+}
